d95b140163a1a3f90570ec6d9dc5949d7eab968c
pepe1973 <bodrogipr@gmail.com>
2025-03-13 21:25:42 +0100

    2025.03.13. Mentés

--- backend/models/room.js
+++ backend/models/room.js
@@ -1,36 +1,39 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-const roomSchema = mongoose.Schema({
-    name : {
-        type: String,
-        required: true
+const roomSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        maxcount: {
+            type: Number,
+            required: true,
+        },
+        phonenumber: {
+            type: Number,
+            required: true,
+        },
+        rentperday: {
+            type: Number,
+            required: true,
+        },
+        imageurls: [],
+        currentbookings: [],
+        type: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        extrak: [{ type: Boolean }],
     },
-    maxcount : {
-        type: Number,
-        required: true
-    },
-    phonenumber : {
-        type: Number,
-        required: true
-    },
-    rentperday : {
-        type: Number,
-        required: true
-    },
-    imageurls : [],
-    currentbookings : [],
-    type : {
-        type: String,
-        required: true
-    },
-    description : {
-        type: String,
-        required: true
+    {
+        timestamps: true,
     }
-
-}, {
-    timestamps : true,
-})
+);
 
 const roomModel = mongoose.model('rooms', roomSchema);
 

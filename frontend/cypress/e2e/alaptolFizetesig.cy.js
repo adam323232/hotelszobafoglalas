81f63c966d6bf601d74421523b0df9ba7e770193
adam323233 <rekotyea32@gmail.com>
2025-03-19 15:46:06 +0100

    15:45 03.19

--- frontend/cypress/e2e/alaptolFizetesig.cy.js
+++ frontend/cypress/e2e/alaptolFizetesig.cy.js
@@ -1,14 +1,26 @@
 describe("Alaptól fizetésig", () => {
   it("Kezdő betöltése", () => {
     cy.visit("http://localhost:5173/");
+    cy.wait(2000);
     cy.get('button[class="landingbtn"]').should("exist");
+    cy.wait(2000);
     cy.get('button[class="landingbtn"]').click();
+    cy.wait(2000);
     cy.get('input[type="text"]').type("user@gmail.com");
+    cy.wait(2000);
     cy.get('div[class="input-container"]')
       .get('input[type="password"]')
       .type("123");
+    cy.wait(2000);
     cy.get('div[class="loginbtn"]')
       .get('button[class="btn btn-primary"]')
       .click();
+    cy.wait(2000);
+    cy.get('div[class="search row"]').should("exist");
+    cy.wait(2000);
+    cy.get('div[class="search row"]')
+      .get('div[class="rangepickerdiv col-md-4"]')
+      .get('')
+    
   });
 });
